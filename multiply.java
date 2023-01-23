1bd6a3a6ac8fd6790cb21ed592aa2a12173dcbd0
PyMass-sem6 <101975897+PyMass2022-23@users.noreply.github.com>
2023-01-23 14:30:47 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+
+    public static void main(String[] args){
+
+        int a = 3;
+        int b = 6;
+        System.out.println("The multiplication is " + (a*b));
+    }
+
